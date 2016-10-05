995dc05be57c058853696e42b790b88ac43bd10b
Yang Liu <yangliu3456@gmail.com>
2016-10-05 19:58:23 +0000

    update

--- app.py
+++ app.py
@@ -15,199 +15,198 @@ import re
 import time
 import sys
 import json
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
-
-address = pd.read_csv('address_book.csv')
 
 cache_dir = 'cache'
 if not os.path.exists(cache_dir):
     os.mkdir(cache_dir)
-    
-@checkpoint(key=string.Template('stride.csv'), work_dir=cache_dir, refresh=True)
-def update_zillow(selected_address):
-    # zillow API function, have 1000 request per day limit
-    # only use after getting small number of interested properties
-
-    from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults, GetUpdatedPropertyDetails
-    # pyzillow 
-    # https://github.com/hanneshapke/pyzillow
-
-    YOUR_ZILLOW_API_KEY = 'X1-ZWz1fcz57zvjt7_6jziz'
-    
-    counter = 0
-    for i in range(len(selected_address)):
-        BBLE = selected_address.BBLE.iloc[i]
-        index = selected_address[selected_address['BBLE'] == BBLE].index.values[0]
-        
-
-        print index, counter
-        if pd.isnull(address.iloc[index].ZILLOW_ID):
-        
-            # sometimes zillow API wrapper dones't accept address/zipcode, even though they are strings
-            try:
-                # convert process street numbers for zillow API wrapper
-                street_num = selected_address.iloc[i]['STREET_NUM']
-                street_name = selected_address.iloc[i]['STREET_NAME']
-
-                # assing address and zipcode
-                address_Z = str(street_num) + ' ' + str(street_name)
-                zipcode_Z = str(selected_address.iloc[i]['ZIP'])
-
-                #use zillow API wrapper to get website link for this property
-                zillow_data = ZillowWrapper(YOUR_ZILLOW_API_KEY)
-                deep_search_response = zillow_data.get_deep_search_results(address_Z, zipcode_Z)
-                result = GetDeepSearchResults(deep_search_response)
-
-                # find the index of the current address
-                BBLE = selected_address.BBLE.iloc[i]
-                index = selected_address[selected_address['BBLE'] == BBLE].index.values[0]
-
-                address['ZILLOW_LINK'].iloc[index] = result.home_detail_link
-                address['ZILLOW_ID'].iloc[index] = result.zillow_id
-                address['LATITUDE'].iloc[index] = result.latitude
-                address['LONGITUTDE'].iloc[index] = result.longitude
-                
-                # check if querried Zillow 1000 times, quit if exceeded limit
-                counter += 1
-                if counter > 1000:
-                    return
-                
-            except:
-                print "error on line", i
-                pass
-    # save zillow data to database
-    filename = 'address_book.csv'
-    address.to_csv(filename, index=False)
 
 @checkpoint(key=string.Template('stride.csv'), work_dir=cache_dir, refresh=True)
-def scrape_zillow(selected_address):
 
-    for i in range(len(selected_address)):
+def selectHouse(budget, built, cusine, food_section, interests, grade):
+	# set home searching parameters
+	budget_max = int(budget)
+	budget_min = int(budget) - 100000
+	built_year = int(built) #built year cutoff
+	grade = int(grade) #average school grade needed
+	#cusine = 'Chinese'
+	food_section = int(food_section) #number of zipcode areas with top restuarant
+	#interest = 'biking'
 
-        BBLE = selected_address.BBLE.iloc[i]
-        index = selected_address[selected_address['BBLE'] == BBLE].index.values[0]
+	# import data from NYC Property Valuation and Assessment data 
+	data = pd.read_csv('TC1.txt')
 
-        if pd.isnull(address.iloc[index].ZILLOW_STATUS) and pd.isnull(address.iloc[index].ZILLOW_ID):
+	# Filter by budget, set as 20% above and below budget
+	budget = data.loc[data['CUR_FV_T'] < budget_max]
+	budget = budget.loc[budget['CUR_FV_T'] > budget_min]
 
-            # scrapying zillow page for school rating
-            r = requests.get(selected_address['ZILLOW_LINK'].iloc[i])
+	# Find places built after specified year
+	after_built_year = budget.loc[budget['YRB'] >= built_year]
+	valid_address = after_built_year[pd.notnull(after_built_year['HNUM_LO'])] # filter valid address for Zillow search
+	price_by_zip = valid_address.groupby(by=['ZIP'])['CUR_FV_T'].mean()
 
-            # parse with BeautifulSoup
+	# imported the NYC restaurant inspection data to look at nearby restaurants
+	food = pd.read_csv('NYC_Restaurant_inspection.csv')
 
-            soup = BeautifulSoup(r.text, "html.parser")
+	# find unique places by last inspection
+	unique_food = food
+	unique_food.drop_duplicates(subset = 'DBA', inplace = True)
 
-            #check if selling
-            sale_tag = soup.find(id = 'home-value-wrapper')
+	# find clean Chinese places
+	clean_food=unique_food.loc[unique_food['GRADE'] == 'A']
+	chinese_food = clean_food.loc[unique_food['CUISINE DESCRIPTION'] == cusine]
 
-            status = ''
+	# group places by zip code
+	food_by_zip=chinese_food.groupby(by=['ZIPCODE'])['GRADE'].count()
 
-            if ('For Sale' in sale_tag.text):
-                status = 'For Sale'
-            elif ('Off Market' in sale_tag.text):
-                status = 'Off Market'
-            elif ('Sold' in sale_tag.text):
-                status = 'Sold'
-            elif ('PENDING' in sale_tag.text):
-                status = 'Pending'
-            else:
-                status = 'Unknown'
+	#combine food and mean price into one dataframe
+	food_and_price = pd.concat([food_by_zip, price_by_zip], axis=1)
 
-            # find id='nearbySchools'
-            rating = soup.find(id = 'nearbySchools')
+	# take out invalid prices
+	short_list= food_and_price[pd.notnull(food_and_price['CUR_FV_T'])]
+	short_list= short_list.sort('GRADE', ascending=False)
 
-            # find 'li' in the table
-            rows = rating.find_all('li')
+	home_with_food = valid_address.loc[valid_address['ZIP'].isin(short_list.head(food_section).index)]
 
-            # find each row by 'div', first char of each striped value is grade
-            # get a list of all three, pk-5, 6-8, 9-12 in random order
-            rate=[]
-            for row in rows[1:4]:
-                score = row.find_all('div')[0].text.strip()
-                rate.append(int(score[0:2]))
-            
-            address.SCHOOL_RATING.iloc[index] = str(rate)
-            address.ZILLOW_STATUS.iloc[index] = status
-        
+	# zillow API function, have 1000 request per day limit
+	# only use after getting small number of interested properties
 
-@checkpoint(key=string.Template('stride.csv'), work_dir=cache_dir, refresh=True)
-def selectHouse(budget, built, cusine, food_section, grade):
-    # set home searching parameters
-    budget_max = int(budget)
-    budget_min = int(budget) - 100000
-    built_year = int(built) #built year cutoff
-    grade = int(grade) #average school grade needed
-    #cusine = 'Chinese'
-    food_section = int(food_section) #number of zipcode areas with top restuarant
-    #interest = 'biking'
+	from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults, GetUpdatedPropertyDetails
+	# pyzillow 
+	# https://github.com/hanneshapke/pyzillow
 
-    # Filter by budget, set as 20% above and below budget
-    budget = address.loc[address['VALUE'] < budget_max]
-    budget = budget.loc[budget['VALUE'] > budget_min]
+	YOUR_ZILLOW_API_KEY = 'X1-ZWz1fcz57zvjt7_6jziz'
 
-    # Find places built after specified year
-    after_built_year = budget.loc[budget['YEAR_BUILT'] >= built_year]
+	zillow_link = []
+	zillow_link_ID = []
+	zillow_lat = []
+	zillow_long = []
 
-    # imported the NYC restaurant inspection data to look at nearby restaurants
-    food = pd.read_csv('food.csv')
+	for i in range(len(home_with_food)):
+	    # sometimes zillow API wrapper dones't accept address/zipcode, even though they are strings
+	    try:
+	        # convert process street numbers for zillow API wrapper
+	        street_num = home_with_food.iloc[i]['HNUM_LO']
 
-    # find preference
-    food_pref = food.loc[food['CUISINE DESCRIPTION'] == cusine]
+	        if (street_num.isdigit() is False):
+	            street_num = ''.join(c for c in street_num if c.isdigit())
+	        number = int(street_num)
 
-    # group places by zip code
-    food_by_zip = food_pref.groupby(by=['ZIPCODE'])['GRADE'].count()
+	        # assing address and zipcode
+	        address = str(number) + ' ' + str(home_with_food.iloc[i]['STR_NAME'])
+	        zipcode = str(int(home_with_food.iloc[i]['ZIP']))
 
-    # only take the top food_section number of zipcodes
-    short_list = food_by_zip.sort_values(ascending=False).head(food_section)
+	        #use zillow API wrapper to get website link for this property
+	        zillow_data = ZillowWrapper(YOUR_ZILLOW_API_KEY)
+	        deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
+	        result = GetDeepSearchResults(deep_search_response)
 
-    selected_address = address.loc[address['ZIP'].isin(list(short_list.index))]
+	        zillow_link.append(result.home_detail_link)
+	        zillow_link_ID.append(result.zillow_id)
+	        zillow_lat.append(result.latitude)
+	        zillow_long.append(result.longitude)
+	    except:
+	        print "error on line", i
+	        pass
 
-    # look at zillow API and update database
-    update_zillow(selected_address)
+	# put list into dataframe
+	links = pd.DataFrame(list(zillow_link))
+	links.columns=['Zillow Links']
+	links['Zillow ID'] = zillow_link_ID
+	links['Latitude'] = zillow_lat
+	links['Longitude'] = zillow_long
 
-    scrape_zillow(selected_address)
+	# scrap from zillow website
+	from bs4 import BeautifulSoup
 
-    # save zillow data to database
-    filename = 'address_book.csv'
-    address.to_csv(filename, index=False)
-    
-    links_sale = selected_address.loc[selected_address['ZILLOW_STATUS'] == 'For Sale']
-    
-    # this is the lat,long array going to be ploted
-    latitudes, longitudes = links_sale['LATITUDE'].tolist(), links_sale['LONGITUTDE'].tolist()
+	ratings = []
+	status = []
 
-    # convert location from string to float
-    latitudes = [float(i) for i in latitudes]
-    longitudes = [float(i) for i in longitudes]
+	for i in range(len(links)):
 
-    # need to generate javascripts for the output html file
-    script = "var markers = ["
+	    rate = []
 
-    for i in range(len(latitudes)):
-        if i < len(latitudes) - 1:
-            script += "[ '"+ links_sale['ZILLOW_ID'].iloc[i] + "', " + str(latitudes[i]) + ", " + str(longitudes[i]) + ", '" + links_sale['ZILLOW_LINK'].iloc[i] + "' ],"
-        else:
-            script += "[ '"+ links_sale['ZILLOW_ID'].iloc[i] + "', " + str(latitudes[i]) + ", " + str(longitudes[i]) + ", '" + links_sale['ZILLOW_LINK'].iloc[i] + "' ]"
-    #close bracket
-    script += "];"
-    print script
-    return script, np.median(latitudes), np.median(longitudes)
+	    #scrapying zillow page for school rating
+	    r = requests.get(links['Zillow Links'][i])
+
+	    #parse with BeautifulSoup
+
+	    soup = BeautifulSoup(r.text, "html.parser")
+
+	    #check if selling
+	    sale_tag = soup.find(id = 'home-value-wrapper')
+
+	    if ('For Sale' in sale_tag.text):
+	        status.append('For Sale')
+	    elif ('Off Market' in sale_tag.text):
+	        status.append('Off Market')
+	    elif ('Sold' in sale_tag.text):
+	        status.append('Sold')
+	    elif ('PENDING' in sale_tag.text):
+	        status.append('Pending')
+	    else:
+	        status.append('Unknown')
+
+	    #find id='nearbySchools'
+	    rating = soup.find(id = 'nearbySchools')
+
+	    #find 'li' in the table
+	    rows = rating.find_all('li')
+
+	    #find each row by 'div', first char of each striped value is grade
+	    #get a list of all three, pk-5, 6-8, 9-12 in random order
+	    rate=[]
+	    for row in rows[1:4]:
+	        score = row.find_all('div')[0].text.strip()
+	        rate.append(int(score[0:2]))
+	    # print rate
+	    ratings.append(rate)
+
+	links['School Rating'] = ratings
+	links['Status'] = status
+	
+	#save the zillow results in a csv
+	today_date = time.strftime("%d_%m_%Y_%H_%M_%S")
+	filename = 'ZillowFiles/Zillow' + today_date + '.csv'
+	links.to_csv(filename)
+	
+	links_sale = links.loc[links['Status'] == 'For Sale']
+	
+	# this is the lat,long array going to be ploted
+	latitudes, longitudes = links_sale['Latitude'].tolist(), links_sale['Longitude'].tolist()
+
+	# convert location from string to float
+	latitudes = [float(i) for i in latitudes]
+	longitudes = [float(i) for i in longitudes]
+
+	#need to generate javascripts for the output html file
+	script = "var markers = ["
+
+	for i in range(len(latitudes)):
+	    if i < len(latitudes) - 1:
+	        script += "[ '"+ links_sale['Zillow ID'].iloc[i] + "', " + str(latitudes[i]) + ", " + str(longitudes[i]) + ", '" + links_sale['Zillow Links'].iloc[i] + "' ],"
+	    else:
+	        script += "[ '"+ links_sale['Zillow ID'].iloc[i] + "', " + str(latitudes[i]) + ", " + str(longitudes[i]) + ", '" + links_sale['Zillow Links'].iloc[i] + "' ]"
+	#close bracket
+	script += "];"
+	print script
+	return script, np.median(latitudes), np.median(longitudes)
 
 def backup():
-    '''
-    This function is used when the main function doesn't geneate proper results
-    '''
-    script = """var markers = [
-                ['2102152090',40.5922,-73.9887,'http://www.zillow.com/homedetails/162-Bay-43-St-1A-Brooklyn-NY-11214/2102152090_zpid/'], 
-                ['112078252',40.58796,-73.984478,'http://www.zillow.com/homedetails/26-Bay-50th-St-3B-Brooklyn-NY-11214/112078252_zpid/'],
-                ['2099301632',40.583672,-73.986096,'http://www.zillow.com/homedetails/171-Bay-52-St-Brooklyn-NY-11214/2099301632_zpid/'],
-                ['30715080',40.60208,-74.007655,'http://www.zillow.com/homedetails/8849-18th-Ave-Brooklyn-NY-11214/30715080_zpid/'],
-                ['68314328',40.599535,-73.994709,'http://www.zillow.com/homedetails/2232-Benson-Ave-3B-Brooklyn-NY-11214/68314328_zpid/']];
-            """
-    lat = 40.58796
-    long = -73.984478
-    return script, lat, long
+	'''
+	This function is used when the main function doesn't geneate proper results
+	'''
+	script = """var markers = [
+				['2102152090',40.5922,-73.9887,'http://www.zillow.com/homedetails/162-Bay-43-St-1A-Brooklyn-NY-11214/2102152090_zpid/'], 
+				['112078252',40.58796,-73.984478,'http://www.zillow.com/homedetails/26-Bay-50th-St-3B-Brooklyn-NY-11214/112078252_zpid/'],
+				['2099301632',40.583672,-73.986096,'http://www.zillow.com/homedetails/171-Bay-52-St-Brooklyn-NY-11214/2099301632_zpid/'],
+				['30715080',40.60208,-74.007655,'http://www.zillow.com/homedetails/8849-18th-Ave-Brooklyn-NY-11214/30715080_zpid/'],
+				['68314328',40.599535,-73.994709,'http://www.zillow.com/homedetails/2232-Benson-Ave-3B-Brooklyn-NY-11214/68314328_zpid/']];
+			"""
+	lat = 40.58796
+	long = -73.984478
+	return script, lat, long
 
 @app.route('/')
 def main():
@@ -215,25 +214,24 @@ def main():
 
 @app.route('/index', methods=['GET','POST'])
 def index():
+	if request.method == 'GET':
+		return render_template('index.html')
+	else:
+		budget = request.form['budget_upper']
+		built = request.form['built']
+		cusine = request.form['food']
+		food_section = request.form['food_selection']
+		# interests = request.form['interests']
+		grade = request.form['school']
+		#print budget, built, cusine, food_section, interests, grade
+		
+		# script, lat, long = selectHouse(budget, built, cusine, food_section, interests, grade)
+		script, lat, long = backup()
 
-    if request.method == 'GET':
-        return render_template('index.html')
-    else:
-        budget = request.form['budget_upper']
-        built = request.form['built']
-        cusine = request.form['food']
-        food_section = request.form['food_selection']
-        # interests = request.form['interests']
-        grade = request.form['school']
-        #print budget, built, cusine, food_section, interests, grade
-        
-        script, lat, long = selectHouse(budget, built, cusine, food_section, grade)
-        # script, lat, long = backup()
-
-        return render_template('index.html', script=script, latitude=lat, longitude=long)
+		return render_template('index.html', script=script, latitude=lat, longitude=long)
   
 if __name__ == '__main__':
-    app.run(host="104.131.11.39", port=33507)
+	app.run(host="104.131.11.39", port=33507)
 
 # injust some changes
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
